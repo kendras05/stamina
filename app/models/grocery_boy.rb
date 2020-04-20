f8d08e5882c54f2b477db0d3861f1676f85925aa
Jeremy Woertink <jeremywoertink@gmail.com>
2020-04-19 20:59:49 -0700

    just making the file readable

--- app/models/grocery_boy.rb
+++ app/models/grocery_boy.rb
@@ -1,15 +1,20 @@
 class GroceryBoy
 
   def initialize(query)
-
     @query = query
+  end
 
+  def browser_args
+    if Rails.env.production?
+      %w[--headless --no-sandbox --disable-dev-shm-usage --disable-gpu --remote-debugging-port=9222]
+    else
+      []
+    end
   end
 
   def search
-
-   browser = Watir::Browser.new :chrome, args: %w[--headless --no-sandbox --disable-dev-shm-usage --disable-gpu --remote-debugging-port=9222] 
     #albertsons
+    browser = Watir::Browser.new :chrome, args: browser_args
 
     browser.goto 'albertsons.com'
 
@@ -23,54 +28,15 @@ class GroceryBoy
 
     search_grid.elements(tag_name: "product-item").each do |product|
 
-
-      
       product_name = product.h3.text
       product_price = product.element(class: "product-price-con").text
       product = {store:"Albertsons", name: product_name, price: product_price}
       product_results.push(product)
-      
 
     end
-   
-    # return the final results as an array of hashes 
-    #albertsons                                      
-=begin
-    #walmart
-
-    browser.goto 'walmart.com' 
-    
-    browser.text_field(id: "global-search-input").value = @query
-    
-    browser.button(id: "global-search-submit").click
-    
-    browser.wait_until {|b| b.element(id: "searchProductResult").exists? }
-
-    items = []
-
-    browser.elements(class: "search-result-gridview-item-wrapper").each do |product|
-
-    sleep 30
-
-    title = product.element(class: "search-result-product-title").text
-  
-#    amount = product.span(class: /price-main/).text
-   
-
-    products = {store: "Walmart", name: title, 
-                #price: amount
-    }
- 
-
-    items.push(products)
-
-    end
-
-=end
 
     browser.close
 
-    product_results[0..10]                           
-#    items[0..10]
-end
+    product_results[0..10]
+  end
 end
